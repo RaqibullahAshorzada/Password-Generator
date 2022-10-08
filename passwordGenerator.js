741fecacc155fb9beafb02250bfc58b4c9d7dda7
Raqibullah <raqibzabul@gmail.com>
2022-10-08 02:40:51 -0700

    added the event to the empty box field

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,8 +1,8 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let passwordOneField = document.getElementById("password1");
+let passwordTwoField = document.getElementById("password2");
 
-let nices1 = document.getElementById("password1")
-let nices2 = document.getElementById("password2")
 
 let passwordOne = '';
 let passwordTwo = '';
@@ -14,13 +14,17 @@ function passwordGenerator() {
     let rand2 = Math.floor(Math.random() * characters.length);
     let pass2 = characters[rand2];
     passwordTwo = passwordTwo + pass2;
-    document.getElementById("password1").innerHTML =  passwordOne;
-    document.getElementById("password2").innerHTML =  passwordTwo;
+    passwordOneField.innerHTML =  passwordOne;
+    passwordTwoField.innerHTML =  passwordTwo;
     
 }
 }
 
-function emptyFields() {
-    document.getElementById("password1").innerHTML = "Passwrod One";
-    document.getElementById("password2").innerHTML = "Password Two";
-}
+
+let fields = document.getElementById("empty");
+fields.addEventListener('click', function() {
+    passwordOneField.innerText =  "Password One";
+    passwordTwoField.innerText = "Password Two";
+})
+
+
